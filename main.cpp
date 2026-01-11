4624379562115b7f8fc36c58c90f9c1e1a9075a2
Franciszek Michalak <michalakfranciszek2005@gmail.com>
2026-01-11 17:22:06 +0100

    Removed build folder content from GH

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(){
     X2D::Cube cube;
     cube.scale(15,15,15);
-    cube.rotate(0.0,1,0.0);
+    cube.rotate(0.5,0.0,0.0);
     //cube.debug();
 
     X2D::Renderer screen{71};
@@ -14,7 +14,7 @@ int main(){
     while(1){
 
         std::cout << "\033[2J\033[1;1H";
-        cube.rotate(0.01,0.0,0.0);
+        cube.rotate(0.0,0.01,0.0);
         //cube.setPosition(Xmath::sin(i)*9, Xmath::cos(i)*9, 0);
 
         screen.render(&cube);
